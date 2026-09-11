54a12b7b19e3cacbb08228a16a408b6e0526c425
Ash-shlimshlam <angelinabayeh9@gmail.com>
2026-09-11 16:17:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+var translations = new Dictionary<string, string>
+{
+    ["un assasin"] = "ein Mörder.",
+    ["en haut"] = "oben.",
+    ["l'ouest"] = "West.",
+    ["le nord"] = "Norden.",
+    ["le sud"] = "Süden."
+};
+Random random = new Random();
+string heading = translations.Keys.ElementAt(random.Next(translations.Count));
+string instructions = translations[heading];
+Console.WriteLine("Übersetze " + instructions);
+string answer;
+answer = Console.ReadLine();
+if (answer == heading) { Console.WriteLine("correct"); } else { Console.WriteLine("false"); }
+
+
