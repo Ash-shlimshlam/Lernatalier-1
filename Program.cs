81dd68a4f7defe2e3e3b5352c08af250ee4be6b7
Ash-shlimshlam <angelinabayeh9@gmail.com>
2026-09-18 16:57:38 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,24 +1,49 @@
 ﻿using System;
 using System.ComponentModel;
 
-var translations = new Dictionary<string, string>
+var Voci = new Dictionary<string, string>
 {
-    ["un assasin"] = "ein Mörder.",
-    ["en haut"] = "oben.",
-    ["l'ouest"] = "West.",
-    ["le nord"] = "Norden.",
-    ["le sud"] = "Süden."
+    ["un auteur/une auteure"] = "ein Autor/eine Autorin",
+    ["international/internationale/internationaux/internationales"] = "International",
+    ["un sport extrem"] = "ein Extremsport",
+    ["une compétition"] = "ein Wettkampf",
+    ["le monde"] = "die Welt",
+    ["entier/entière"] = "ganz,völlig",
+    ["exister"] = "existieren",
+    ["un monument"] = "ein Monument",
+    ["ouvert/ouverte"] = "offnen/geöffnet",
+    ["la lumière"] = "das Licht"
 };
-do
+
+    do
 {
     Random random = new Random();
-    string heading = translations.Keys.ElementAt(random.Next(translations.Count));
-    string instructions = translations[heading];
-    Console.WriteLine("Übersetze " + instructions);
+    string French_translations = Voci.Keys.ElementAt(random.Next(Voci.Count));
+    string German_translations = Voci[French_translations];
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine(" //Übersetze: " + German_translations + "//");
     string answer;
     answer = Console.ReadLine();
-    if (answer == heading) { Console.WriteLine("correct"); translations.Remove(heading); } else { Console.WriteLine("false " + heading); }
-} while (translations.Count > 0);
+   
+    if (answer == French_translations) 
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("richtig!"); Voci.Remove(French_translations);
+     
+     
+        
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine ("falsch, die Antwort wäre; " + French_translations);
+      
+    }
+} while (Voci.Count > 0);
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("Sie haben alle Wörter erfolgreich übersetzt!");
+
+
 
     
 
